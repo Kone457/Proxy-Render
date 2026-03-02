bfed7766df499d89ac78dd059a4518775c1df18a
Carlos <supernumerovirtual@gmail.com>
2026-03-01 21:26:44 -0500

    Actualizar server.js

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 
-
 app.use("/", createProxyMiddleware({
-  target: "https://olivia.hidencloud.com:24711",
+  target: "http://olivia.hidencloud.com:24711/",
   changeOrigin: true,
-  ws: true 
+  ws: true,
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Cache-Control'] = 'no-cache';
+  }
 }));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Proxy corriendo en Render...");
+app.listen(10025, "0.0.0.0", () => {
+  console.log("Proxy running on port 5000...");
 });
